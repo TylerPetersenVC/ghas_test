a8b51b852b863d9cbf5d8f2786c2122c2760b9e7
Tyler Petersen <Tyler.Petersen@ventura.org>
2024-08-05 12:01:08 -0700

    updating output directory

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a63ecba4affd03275f2933b3f4687c13d8403f1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af3c5c988cce497f91eef6546749fe524613d825")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
