65a13a58e0decc2cc4d661cdc67c20fba7338aef
TylerPetersenVC <tyler.petersen@ventura.org>
2024-08-15 11:41:34 -0700

    resolving data issues for dev vs prod

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dd0756140e9cf97c68b37080f2f80ac721a0ec2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17f08a959fa3cb90ecf56cce6fc920cb968170d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
