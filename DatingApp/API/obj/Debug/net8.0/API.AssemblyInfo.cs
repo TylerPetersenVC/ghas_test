b70207e101ea4b765c6e1c4e75b419d0e9feba47
TylerPetersenVC <tyler.petersen@ventura.org>
2024-08-15 12:24:28 -0700

    modifying login for environment

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+624e6ea803cbc33aca0be6d87dbaf5d7421dbe44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc21cf70691db7507ec292db8caa5e0abcb50edc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
