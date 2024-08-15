4294820fa32ea2466ca67e1e843296b39220dd61
TylerPetersenVC <tyler.petersen@ventura.org>
2024-08-15 16:53:56 -0700

    added register form

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -1,26 +1,20 @@
-import { NgFor } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
-import { environment } from '../environments/environment';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
   private accountService = inject(AccountService);
-  title = 'DatingApp';
-  users: any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -29,16 +23,5 @@ export class AppComponent implements OnInit {
     if (!userString) return;
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
-  }
-
-  // 'https://localhost:4200/api/users' --> Dev
-  // 'http://datingapptylerp/api/mockusers' --> Prod
-  getUsers() {
-    // const envUsers = environment.production ? '/mockusers' : '/users';
-    this.http.get('http://localhost:5001/api/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request has completed.'),
-    });
   }
 }
