27dcca9c81c776bf06e44c4938822f1ab85e3160
Tyler Petersen <Tyler.Petersen@ventura.org>
2024-09-11 17:17:47 -0700

    Added user info page

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4c3af6c339a707844dcc5559bc57938d7eaaa0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+817a2a058605f1773cdc1f0b03fe965745d8a16e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
