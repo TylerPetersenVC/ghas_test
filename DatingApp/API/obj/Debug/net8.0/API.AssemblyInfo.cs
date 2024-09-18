7f7aa886c496113ad0041c5ecd1b4907cdbf3c3c
TylerPetersenVC <tyler.petersen@ventura.org>
2024-09-18 16:59:34 -0700

    User photo gallery functionality, add/update photos

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7996168df75191e427734de2ce460d2c9957a079")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f71efa64642db8b37c5c9923e63cfab37927479")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
