e0ee05c24fedcf6d36b64c3610248ee56a692fec
TylerPetersenVC <tyler.petersen@ventura.org>
2024-09-19 12:18:58 -0700

    updating appsettings.production.json, adding connection string

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd36a6dd29f032820f893f126dac2a6109c80d19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e0c4b956e9dae2068a7392211ddb0b78b09b63d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
