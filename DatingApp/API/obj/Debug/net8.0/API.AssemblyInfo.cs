ca43683d675c6fb546bdbc7e06cd90cacd7c6da9
TylerPetersenVC <tyler.petersen@ventura.org>
2024-09-19 17:09:42 -0700

    Added validation to register form

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe9f5174e58d53f98c643661274e70b5b9b2c81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e52474d6270fc73fe36c8dd8528a0586a427a138")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
