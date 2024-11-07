4bc2f99e48f1575d4fd637f9af657e271c3f210e
TylerPetersenVC <tyler.petersen@ventura.org>
2024-11-07 10:12:34 -0800

    database update

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df86f0ff27df3efa3ded201f816249553857aebe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8998dbf7cb56f3f86224cfdeee30b1fe88c00e0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
