451f46a5a24e93f27de6dc9e09f0e32fea78f275
Tyler Petersen <Tyler.Petersen@ventura.org>
2025-06-11 14:22:48 -0700

    test

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8998dbf7cb56f3f86224cfdeee30b1fe88c00e0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bc2f99e48f1575d4fd637f9af657e271c3f210e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
